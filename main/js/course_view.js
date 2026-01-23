cbe5db3c9913e3841fe5e7998934a204b3df5590
ephr3on <92059831+ephr3on@users.noreply.github.com>
2026-01-23 18:34:26 +0100

    reorder the buttins of the session cards

--- main/js/course_view.js
+++ main/js/course_view.js
@@ -114,16 +114,6 @@ function loadSessions() {
                         Session-Link
                     </button>
 
-                    <button class="btn btn-outline btn-sm">
-
-                        <svg class="icon" viewBox="0 0 24 24">
-                            <path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"></path>
-                            <path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                        </svg>
-
-                        Edit Session
-                    </button>
-
 
                     <button class="btn btn-sm"
                         style="background-color: ${session.isActive ? '#dc2626' : '#16a34a'}; 
@@ -137,6 +127,16 @@ function loadSessions() {
                         </svg>
 
                         ${session.isActive ? 'Close Session' : 'Reopen Session'}
+                    </button>
+
+
+                    <button class="btn btn-outline btn-sm">
+                        <svg class="icon" viewBox="0 0 24 24">
+                            <path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"></path>
+                            <path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                        </svg>
+
+                        Edit Session
                     </button>
 
 
