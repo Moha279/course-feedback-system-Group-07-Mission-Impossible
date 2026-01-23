8d9e34987011f4893ca87d8189c1bd1651fd23e4
ephr3on <92059831+ephr3on@users.noreply.github.com>
2026-01-23 18:37:26 +0100

    addint background color to .session-item in Student dashboard

--- main/js/course_view.js
+++ main/js/course_view.js
@@ -140,9 +140,8 @@ function loadSessions() {
                     </button>
 
 
-                    <button class="btn btn-outline btn-sm" style="color: #dc2626;"
+                    <button class="btn btn-outline btn-sm" style="color: #dc2626; margin-left: auto;"
                             onclick="confirmDeleteSession('${session.id}')">
-
                         <svg class="icon" viewBox="0 0 24 24">
                             <path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"></path>
                         </svg>
